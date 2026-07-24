c6ed96a67945ffda226a35df0f39512684c7dc2d
prashant2906pandey <77741428+prashant2906pandey@users.noreply.github.com>
2026-07-24 23:01:27 +0530

    hamburger menu implemented

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
 console.log("Rangvana Website Loaded Successfully 🚀");
+const menuBtn = document.querySelector(".menu-btn");
+const navLinks = document.querySelector(".nav-links");
+
+menuBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
