ed2c82f1cd50391d1650753e13793f7432f65717
prashant2906pandey <77741428+prashant2906pandey@users.noreply.github.com>
2026-07-25 14:33:38 +0530

    form enable

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ const navLinks = document.querySelector(".nav-links");
 
 menuBtn.addEventListener("click", () => {
     navLinks.classList.toggle("active");
+    menuBtn.classList.toggle("open");
 });
 
 // Gallery Lightbox
@@ -125,3 +126,61 @@ AOS.init({
     duration:1000,
     once:true
 });
+
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+    if(window.scrollY > 50){
+        header.classList.add("scrolled");
+    }else{
+        header.classList.remove("scrolled");
+    }
+});
+const sections = document.querySelectorAll("section");
+const navItems = document.querySelectorAll(".nav-links a");
+
+window.addEventListener("scroll",()=>{
+
+    let current="";
+
+    sections.forEach(section=>{
+        const sectionTop = section.offsetTop - 120;
+
+        if(scrollY >= sectionTop){
+            current = section.getAttribute("id");
+        }
+    });
+
+    navLinks.forEach(link=>{
+        link.classList.remove("active");
+
+        if(link.getAttribute("href") === "#" + current){
+            link.classList.add("active");
+        }
+    });
+
+});
+const classForm = document.querySelector("#classForm");
+const successMsg = document.querySelector("#successMsg");
+
+
+if(classForm){
+
+classForm.addEventListener("submit",(e)=>{
+
+    e.preventDefault();
+
+
+    const name = document.querySelector("#name").value;
+
+
+    successMsg.innerHTML =
+    `Thank you ${name}! Your class booking request has been received 🎨`;
+
+
+    classForm.reset();
+
+
+});
+
+}
